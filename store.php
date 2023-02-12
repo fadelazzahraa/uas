eb3f6146a65c70807eb6050091e703b29c6e5213
Fadel Azzahra <fadelazzahra@rocketmail.com>
2023-02-12 23:01:57 +0700

    done

--- store.php
+++ store.php
@@ -24,8 +24,7 @@ $products = $result['data'];
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <link href="dist/bootstrap.min.css" rel="stylesheet">
 
   <link rel="icon" href="image/favicon.ico" type="image/x-icon">
   <title>Store | Sanapati Food Store</title>
@@ -102,13 +101,11 @@ $products = $result['data'];
   <!-- End of Footer -->
 
   <!-- Script -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
-    crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
-    crossorigin="anonymous"></script>
+  <script src="dist/bootstrap.bundle.min.js"></script>
+  <script src="popper.min.js"></script>
   <!-- End of Script -->
+
+  <link href="dist/bootstrap.min.css" rel="stylesheet">
 </body>
 
 </html>
